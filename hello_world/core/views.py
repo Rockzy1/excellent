1c1256514b440336be976d13eb8c668045507efb
Rockzy <88431507+Rockzy1@users.noreply.github.com>
2025-03-28 07:59:19 +0100

    Update views.py

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    context = {
-        "title": "Django example",
-    }
-    return render(request, "index.html", context)
+   return render("Hello World)
